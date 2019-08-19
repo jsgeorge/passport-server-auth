231fbdb5c3f69e1460c418f642bc93e23a2dd794
George M <gmeyer1000@yahoo.com>
2019-08-19 08:47:39 -0700

    refine

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const app = express();
 const router = require("./router");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 //require("dotenv").config();
 
 //mongo DB
@@ -15,6 +15,7 @@ mongoose.connect(
 
 //App setup
 app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
